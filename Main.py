0107828e4f856a0a62209053d5225ed7e74cd22a
Mario Rocha <roc23501@uvg.edu.gt>
2025-08-29 11:48:39 -0600

    Feat: README && changes in the Main

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-Class CRTSimulation: 
+import nympy as np
+
+class CRTSimulation: 
     def __init__(self):
         # Constantes físicas del CRT
         self.ELECTRON_CHARGE = 1.602e-19  # Coulombs
@@ -28,4 +30,39 @@ Class CRTSimulation:
         self.is_running = False
         self.trail_points = []
         self.current_position = {'x': 0, 'y': 0}
+
+
+        self.setup_interface()
+
+    def calculate_initial_velocity(self):
+       energia_cinetica = self.ELECTRON_CHARGE * self.acceleration_voltage
+       return np.sqrt(2 * energia_cinetica / self.ELECTRON_MASS)
+
+
+   def calculate_deflection (self,voltage, PLATE_LENGTH, PLATE_SEPARATION, initial_velocity):
+       electric_field = voltage / PLATE_SEPARATION
+       force = self.ELECTRON_CHARGE * electric_field
+       acceleration = force/ self.ELECTRON_MASS
+       time_in_plates = PLATE_LENGTH / initial_velocity
+       
+       deflection_in_plates = 0.5 * acceleration * time_in_plates **2 
+       velocity_after_plates = acceleration * time_in_plates
+       time_to_screen = (self.CRT_LENGTH - PLATE_LENGTH) / initial_velocity
+       
+       additional_deflection = velocity_after_plates * time_to_screen
+
+       return deflection_in_plates + additional_deflection
+   
+   
+    def get_voltages(self, t): 
+        if self.mode == 'manual':
+            return{'vx': self.manual_vx, 'vy': self.manual_vy}
+        else: 
+            vx = (self.sine_params['amplitude_x'] * 
+                  np.sin(2 * np.pi * self.sine_params['frequency_x'] * t + 
+                         np.radians(self.sine_params['phase_x'])))
+            vy = (self.sine_params['amplitude_y'] * 
+                  np.sin(2 * np.pi * self.sine_params['frequency_y'] * t + 
+                         np.radians(self.sine_params['phase_y'])))
+            return {'vx': vx, 'vy': vy}
         
