e04b5c0cebd4f13efe55547254afd05c7558b43f
Mario Rocha <roc23501@uvg.edu.gt>
2025-08-29 12:21:30 -0600

    Feat: Lateral view

--- Main.py
+++ Main.py
@@ -66,3 +66,54 @@ class CRTSimulation:
                          np.radians(self.sine_params['phase_y'])))
             return {'vx': vx, 'vy': vy}
         
+    def setup_interface(self):
+        plt.style.use('dark_background')
+        
+        self.fig = plt.figure(figsize=(16, 10))
+        self.fig.suptitle('simulación de Tubo de Rayos Catódicos (CRT)', fontsize = 16, color = 'white')
+
+        gs = self.fig.add_gridspec(3, 4, height_ratios=[2, 2, 1], width_ratios=[1, 1, 1, 1])
+        
+        self.ax_lateral = self.fig.add_subplot(gs[0, 0])
+        self.setup_lateral_view()
+        
+        self.ax_superior = self.fig.add_subplot(gs[0, 1])
+        self.setup_superior_view()
+        
+        self.ax_screen = self.fig.add_subplot(gs[0, 2:])
+        self.setup_screen_view()
+        
+        self.ax_voltage_x = self.fig.add_subplot(gs[1,0:s])
+        self.ax_voltage_y = self.fig.add_subplot(gs[1,2:])
+        
+        self.setup_voltage_plots()
+        
+        self.setup_controls()
+        
+        self.ani = animation.FuncAnimation(
+            self.fig, self.animate, interval=50, blit=False
+        )
+        
+        plt.tight_layout()
+        plt.subplots_adjust(bottom=0.25)
+        
+        def setup_lateral_view(self):
+            self.ax_lateral.set_xlim(0,300)
+            self.ax_lateral.set_ylim(0,200)
+            self.ax_lateral.set_title('Vista Lateral (deflexion en Y)', color = 'cyan')
+            self.ax_lateral.set_facecolor('black')
+
+            #cannon de electrones
+            cannon = plt.Rectangle ((20, 85), 30, 30, fill=False, edgecolor='white', linewidth=2)
+            self.ax_lateral.add_patch(cannon)
+            
+            #placas de deflexión vertical 
+            self.ax_lateral.plot ([120, 180], [60, 60], 'white', linewidth=3, label='Placa superior')
+            self.ax_lateral.plot ([120, 180], [140, 140], 'white', linewidth=3, label='Placa inferior')
+            
+            #pantalla 
+            self.beam_lateral, = self.ax_lateral.plot([], [], 'white', linewidth=2, label='Haz de electrones')
+            self.dot_lateral, = self.ax_lateral.plot([], [], 'ro', markersize=5, label='Posición del electrón')
+            
+            self.ax_lateral.set_aspect('equal')
+            self.ax_lateral.set_grid(True, alpha=0.3)
